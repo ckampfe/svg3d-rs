a60d3882b34277c5484676431130352f1deb0baf
Clark Kampfe <clark.kampfe@gmail.com>
2019-07-14 21:46:08 -0500

    many refactorings, add cube

--- src/main.rs
+++ src/main.rs
@@ -4,25 +4,47 @@ use svg::node::element::{Group, Polygon};
 use svg::Document;
 
 type StyleMap<V> = HashMap<String, V>;
-type Faces = Vec<Vec<Point3<f32>>>;
+type Face = [Point3<f32>; 3];
 
-struct Mesh<T> {
-    faces: Faces,
-    style: HashMap<String, String>,
-    shader: Option<Box<Fn(usize, f32) -> StyleMap<T>>>,
+fn winding(face: &Face) -> f32 {
+    let [p1, p2, p3] = face;
+    (p2 - p1).cross(&(p3 - p1))[2]
 }
 
-impl<T> Mesh<T> {
-    fn new(faces: Faces) -> Self {
-        Mesh {
-            faces,
-            style: HashMap::new(),
-            shader: None,
-        }
-    }
+fn cube() -> Vec<Face> {
+    let vertices = [
+        Point3::new(-0.5, -0.5, -0.5),
+        Point3::new(-0.5, 0.5, -0.5),
+        Point3::new(0.5, 0.5, -0.5),
+        Point3::new(0.5, -0.5, -0.5),
+        Point3::new(-0.5, -0.5, 0.5),
+        Point3::new(-0.5, 0.5, 0.5),
+        Point3::new(0.5, 0.5, 0.5),
+        Point3::new(0.5, -0.5, 0.5),
+    ];
+
+    let indices = [
+        [0, 3, 1],
+        [1, 3, 2],
+        [0, 1, 5],
+        [0, 5, 4],
+        [1, 2, 5],
+        [6, 5, 2],
+        [7, 6, 2],
+        [7, 2, 3],
+        [7, 3, 0],
+        [4, 7, 0],
+        [5, 6, 4],
+        [4, 6, 7],
+    ];
+
+    indices
+        .iter()
+        .map(|group| [vertices[group[0]], vertices[group[1]], vertices[group[2]]])
+        .collect()
 }
 
-fn octahedron() -> Vec<Vec<Point3<f32>>> {
+fn octahedron() -> Vec<Face> {
     let f: f32 = 2.0f32.sqrt() / 2.0;
     let vertices = [
         Point3::new(0.0, -1.0, 0.0),
@@ -46,8 +68,69 @@ fn octahedron() -> Vec<Vec<Point3<f32>>> {
 
     indices
         .iter()
-        .map(|group| group.iter().map(|index| vertices[*index]).collect())
+        .map(|group| [vertices[group[0]], vertices[group[1]], vertices[group[2]]])
         .collect()
+}
+
+fn icosahedron() -> Vec<Face> {
+    let vertices = [
+        Point3::new(0.000, 0.000, 1.000),
+        Point3::new(0.894, 0.000, 0.447),
+        Point3::new(0.276, 0.851, 0.447),
+        Point3::new(-0.724, 0.526, 0.447),
+        Point3::new(-0.724, -0.526, 0.447),
+        Point3::new(0.276, -0.851, 0.447),
+        Point3::new(0.724, 0.526, -0.447),
+        Point3::new(-0.276, 0.851, -0.447),
+        Point3::new(-0.894, 0.000, -0.447),
+        Point3::new(-0.276, -0.851, -0.447),
+        Point3::new(0.724, -0.526, -0.447),
+        Point3::new(0.000, 0.000, -1.000),
+    ];
+
+    let indices = [
+        [0, 1, 2],
+        [0, 2, 3],
+        [0, 3, 4],
+        [0, 4, 5],
+        [0, 5, 1],
+        [11, 7, 6],
+        [11, 8, 7],
+        [11, 9, 8],
+        [11, 10, 9],
+        [11, 6, 10],
+        [1, 6, 2],
+        [2, 7, 3],
+        [3, 8, 4],
+        [4, 9, 5],
+        [5, 10, 1],
+        [6, 7, 2],
+        [7, 8, 3],
+        [8, 9, 4],
+        [9, 10, 5],
+        [10, 6, 1],
+    ];
+
+    indices
+        .iter()
+        .map(|group| [vertices[group[0]], vertices[group[1]], vertices[group[2]]])
+        .collect()
+}
+
+struct Mesh<'a, T> {
+    faces: &'a [Face],
+    style: HashMap<String, String>,
+    shader: Option<Box<Fn(usize, f32) -> StyleMap<T>>>,
+}
+
+impl<'a, T> Mesh<'a, T> {
+    fn new(faces: &'a [Face]) -> Self {
+        Mesh {
+            faces,
+            style: HashMap::new(),
+            shader: None,
+        }
+    }
 }
 
 struct Camera {
@@ -90,24 +173,24 @@ impl Default for Viewport {
     }
 }
 
-struct Scene<T> {
-    meshes: Vec<Mesh<T>>,
+struct Scene<'a, T> {
+    meshes: &'a [Mesh<'a, T>],
 }
 
-impl<T> Scene<T> {
-    fn new(meshes: Vec<Mesh<T>>) -> Self {
+impl<'a, T> Scene<'a, T> {
+    fn new(meshes: &'a [Mesh<T>]) -> Self {
         Scene { meshes }
     }
 }
 
-struct View<T> {
+struct View<'a, T> {
     camera: Camera,
-    scene: Scene<T>,
+    scene: Scene<'a, T>,
     viewport: Viewport,
 }
 
-impl<T> View<T> {
-    fn new(camera: Camera, scene: Scene<T>) -> Self {
+impl<'a, T> View<'a, T> {
+    fn new(camera: Camera, scene: Scene<'a, T>) -> Self {
         View {
             camera,
             scene,
@@ -116,12 +199,12 @@ impl<T> View<T> {
     }
 }
 
-struct Engine<T> {
-    views: Vec<View<T>>,
+struct Engine<'a, T> {
+    views: &'a [View<'a, T>],
 }
 
-impl<T> Engine<T> {
-    fn new(views: Vec<View<T>>) -> Self {
+impl<'a, T> Engine<'a, T> {
+    fn new(views: &'a [View<T>]) -> Self {
         Engine { views }
     }
 
@@ -132,10 +215,10 @@ impl<T> Engine<T> {
             .set("width", 512)
             .set("height", 512);
 
-        for view in &self.views {
+        for view in self.views {
             let projection =
                 view.camera.projection.to_homogeneous() * view.camera.view.to_homogeneous();
-            for mesh in &view.scene.meshes {
+            for mesh in view.scene.meshes {
                 document = document.add(self.create_group(projection, &view.viewport, mesh));
             }
         }
@@ -147,33 +230,27 @@ impl<T> Engine<T> {
         let faces = &mesh.faces;
         // let default_style = &mesh.style;
 
-        let with_w = faces.iter().map(|face| {
-            vec![
-                face[0].to_homogeneous(),
-                face[1].to_homogeneous(),
-                face[2].to_homogeneous(),
+        // from xyz to xyzw
+        let with_w = faces.iter().map(|[p1, p2, p3]| {
+            [
+                p1.to_homogeneous(),
+                p2.to_homogeneous(),
+                p3.to_homogeneous(),
             ]
         });
 
-        let projected = with_w.map(|face| {
-            vec![
-                projection * face[0],
-                projection * face[1],
-                projection * face[2],
+        let projected =
+            with_w.map(|[p1, p2, p3]| [projection * p1, projection * p2, projection * p3]);
+
+        let points_by_w = projected.map(|[p1, p2, p3]| {
+            [
+                Point3::new(p1.x / p1.w, p1.y / p1.w, p1.z / p1.w),
+                Point3::new(p2.x / p2.w, p2.y / p2.w, p2.z / p2.w),
+                Point3::new(p3.x / p3.w, p3.y / p3.w, p3.z / p3.w),
             ]
         });
 
-        let points_by_w = projected.map(|face| {
-            let [p1, p2, p3] = [face[0], face[1], face[2]];
-
-            vec![
-                Point3::new(p1[0] / p1[3], p1[1] / p1[3], p1[2] / p1[3]),
-                Point3::new(p2[0] / p2[3], p2[1] / p2[3], p2[2] / p2[3]),
-                Point3::new(p3[0] / p3[3], p3[1] / p3[3], p3[2] / p3[3]),
-            ]
-        });
-
-        let viewport_transformed: Vec<Vec<Point3<f32>>> = points_by_w
+        let viewport_transformed: Vec<Face> = points_by_w
             .map(|mut face| {
                 face.iter_mut().for_each(|point| {
                     point.x = (1.0 + point.x) * viewport.width / 2.0 + viewport.minx;
@@ -190,42 +267,42 @@ impl<T> Engine<T> {
                 let z_centroid = face.iter().map(|point| point[2]).sum::<f32>() / 3.0;
                 (face, z_centroid)
             })
-            .collect::<Vec<(Vec<Point3<f32>>, f32)>>();
+            .collect::<Vec<(Face, f32)>>();
 
         z_centroids
-            .sort_unstable_by(|a, b| a.1.partial_cmp(&b.1).unwrap_or(std::cmp::Ordering::Less));
+            .sort_unstable_by(|a, b| a.1.partial_cmp(&b.1).unwrap_or(std::cmp::Ordering::Equal));
 
         let mut sorted_faces = z_centroids
             .into_iter()
             .map(|(face, _)| face)
-            .collect::<Vec<Vec<Point3<f32>>>>();
+            .collect::<Vec<Face>>();
 
         sorted_faces.reverse();
 
         let mut group = Group::new()
             .set("fill", "white")
-            .set("fill-opacity", 0.75)
+            // .set("fill-opacity", 0.75)
+            .set("fill-opacity", 1.0)
             .set("stroke", "black")
             .set("stroke-linejoin", "round")
             .set("stroke-width", 0.005);
 
         for face in sorted_faces {
-            let [p0, p1, p2] = [face[0], face[1], face[2]];
-            let winding = (p1 - p0).cross(&(p2 - p0))[2];
+            let winding = winding(&face);
             // let style = shader(1, winding);
 
-            // there is no first-class points method, PR this maybe?
-            let polygon = Polygon::new().set(
-                "points",
-                [
-                    [face[0].x.to_string(), face[0].y.to_string()].join(","),
-                    [face[1].x.to_string(), face[1].y.to_string()].join(","),
-                    [face[2].x.to_string(), face[2].y.to_string()].join(","),
-                ]
-                .join(" "),
-            );
+            if winding > 0.0 {
+                // there is no first-class points method, PR this maybe?
+                let polygon = Polygon::new().set(
+                    "points",
+                    face.iter()
+                        .map(|point| [point.x.to_string(), point.y.to_string()].join(","))
+                        .collect::<Vec<String>>()
+                        .join(" "),
+                );
 
-            group = group.add(polygon)
+                group = group.add(polygon)
+            }
         }
 
         group
@@ -243,13 +320,16 @@ fn main() {
         Vector3::y(),
     );
 
-    let mesh = Mesh::<String>::new(
-        octahedron()
-            .iter()
-            .map(|face| vec![15.0 * face[0], 15.0 * face[1], 15.0 * face[2]])
-            .collect(),
-    );
-    let view = View::new(camera, Scene::new(vec![mesh]));
-    let engine = Engine::new(vec![view]);
+    let octahedron: Vec<Face> = octahedron()
+        .iter()
+        .map(|face| [15.0 * face[0], 15.0 * face[1], 15.0 * face[2]])
+        .collect();
+
+    let mesh = Mesh::<String>::new(&octahedron);
+    let meshes = [mesh];
+
+    let view = View::new(camera, Scene::new(&meshes));
+    let views = [view];
+    let engine = Engine::new(&views);
     engine.render("octahedron.svg".to_string())
 }
